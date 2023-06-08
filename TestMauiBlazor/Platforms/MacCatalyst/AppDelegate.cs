5f5f9416795014e97d41c8001e826563cc5fb8a0
Luca Giuliani <giulianil@microsoft.com>
2023-06-08 13:15:12 +0200

    Add project files.

---
+++ TestMauiBlazor/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TestMauiBlazor
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
